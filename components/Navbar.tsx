3f9a2bb7fa306465d565a9fac120bb835791c268
Franco Ciprian <franco_ciprian@hotmail.com>
2023-07-18 20:52:46 -0300

    Update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import { buttonVariants } from "./ui/Button";
 import { Icons } from "./Icons";
+import { getAuthSession } from "@/lib/auth"
+import { AccountNav } from "./AccountNav";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getAuthSession()
+
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
       <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
@@ -12,12 +16,16 @@ const Navbar = () => {
           <p className='hidden text-zinc-700 text-sm font-medium md:block'>Franddit</p>
         </Link>
 
-      {/* Search bar */}
-  
-      {/* Actions */}
-        <Link href='/sign-in' className={buttonVariants()}>
-          Sign In
-        </Link>
+        {/* Search bar */}
+
+        {/* Actions */}
+        {session?.user ? (
+          <AccountNav user={session.user} />
+        ) : (
+          <Link href='/sign-in' className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   )

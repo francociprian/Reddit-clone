532d96c6e061726167ea6d194f27008662c4412a
Franco Ciprian <franco_ciprian@hotmail.com>
2023-07-18 20:19:43 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
     },
+    experimental: {
+        appDir: true
+    }
 }
 
 module.exports = nextConfig

a60596d2c61bf9da762d6db40a6c5df281a26868
Franco Ciprian <franco_ciprian@hotmail.com>
2023-07-18 09:18:40 -0300

    Update RootLayout,add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import '@/styles/globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html 
+      lang="en"
+      className={cn('bg-white text-slate-900 antialiased', inter.className)}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        <div className='container max-w-7xl mx-auto h-full pt-12'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }

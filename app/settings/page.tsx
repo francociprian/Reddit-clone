7d86f0c184fc01b1deda6d9d4ef31cac8b7c47b6
Franco Ciprian <franco_ciprian@hotmail.com>
2023-07-20 09:37:34 -0300

    Create page settings

---
+++ app/settings/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface pageProps { }
+
+const page: FC<pageProps> = ({ }) => {
+  return (
+    <><h1 className='font-bold text-3xl md:text-4xl'>Settings</h1></>
+  )
+}
+
+export default page
